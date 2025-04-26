30ba759938499610a84644d4771c73d1adaf9719
AustineNamayi21 <austine.namayi@strathmore.edu>
2025-04-26 14:38:55 +0300

    html and css files

---
+++ home.js
@@ -0,0 +1,30 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Navbar background change on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('bg-white');
+        navbar.classList.add('navbar-light');
+        navbar.classList.remove('bg-light');
+    } else {
+        navbar.classList.remove('bg-white');
+        navbar.classList.remove('navbar-light');
+        navbar.classList.add('bg-light');
+    }
+});
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
